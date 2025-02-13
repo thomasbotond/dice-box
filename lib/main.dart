e34c2e13fac867076ea91276070df450ab45d6e6
Thomas Botond <thomas.botond@icloud.com>
2025-02-13 14:24:28 +0100

    Create basic home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.amber,
+                Colors.yellow,
+              ],
+            ),
+          ),
+          child: Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 24,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
